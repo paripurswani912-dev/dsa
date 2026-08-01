19f378e5ddf29c71cac346a66639c3f3fc67a9a1
Pari <paripurswani912@gmail.com>
2026-08-01 17:42:16 +0530

    dsa basics

---
+++ basics.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void printNumber() {
+        int variable;
+        cin>>variable;
+        cout<<variable;
+        
+
+    }
+};
+int main() {
+    Solution obj;
+    obj.printNumber();
+    return 0;
+}

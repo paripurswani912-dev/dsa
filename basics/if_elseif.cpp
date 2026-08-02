13184dee7cf68843dbba976ebd5c59ba379a1605
Pari <paripurswani912@gmail.com>
2026-08-02 23:43:26 +0530

    if elseif

---
+++ basics/if_elseif.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void studentGrade(int marks) {
+
+        if(marks>=90){
+            cout<<"Grade A";
+        }
+        else if(marks>=70){
+            cout<<"Grade B";
+        }
+        else if(marks>=50){
+            cout<<"Grade C";
+        }
+          else if(marks>=35){
+            cout<<"Grade D";
+        }
+        else{
+            cout<<"Fail";
+        }
+
+
+    }
+};
+int main() {
+    Solution obj;
+    int marks;
+    cin>>marks;
+    obj.studentGrade(marks);
+    return 0;
+}

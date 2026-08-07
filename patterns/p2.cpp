f7b7875574982909eb51d70c2aebebf8290f25fc
Pari <paripurswani912@gmail.com>
2026-08-07 19:21:10 +0530

    pattern 2

---
+++ patterns/p2.cpp
@@ -0,0 +1,34 @@
+//Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+//*
+
+//**
+
+//***
+
+//****
+
+//*****
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void pattern2(int n) {
+        int i,j;
+        for(i=0;i<n;i++){
+            
+            for(j=0;j<=i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+            
+        }
+    }
+};
+int main() {
+    Solution obj;
+    int n;
+    cin>>n;
+    obj.pattern2(n);
+    return 0;
+}

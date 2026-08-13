20b51063dfc8cd1fce9c212686e3cd02b484c326
Pari <paripurswani912@gmail.com>
2026-08-13 18:29:19 +0530

    pattern 11

---
+++ patterns/p11.cpp
@@ -0,0 +1,34 @@
+//Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+//1 
+//0 1 
+//1 0 1 
+//0 1 0 1 
+//1 0 1 0 1
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void pattern11(int n) {
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    cout<<"1 ";
+
+                }
+                else{
+                    cout<<"0 ";}
+            }
+            cout<<endl;
+        }
+
+    }
+};
+int main() {
+    Solution obj;
+    int n;
+    cin >> n;
+    obj.pattern11(n);
+    return 0;
+}

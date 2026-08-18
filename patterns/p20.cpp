62a42551d438f67b86c2feb8a8319f7d23fff0c3
Pari <paripurswani912@gmail.com>
2026-08-18 08:48:20 +0530

    pattern 20

---
+++ patterns/p20.cpp
@@ -0,0 +1,60 @@
+//Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+//*        *
+//**      **
+//***    ***
+//****  ****
+//**********
+//****  ****
+//***    ***
+//**      **
+//*        *
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void pattern20(int n) {
+        int i, j;
+
+        // Upper half
+        for(i = 1; i <= n; i++) {
+
+            for(j = 0; j < i; j++) {
+                cout << "*";
+            }
+
+            for(j = 0; j < 2 * (n - i); j++) {
+                cout << " ";
+            }
+
+            for(j = 0; j < i; j++) {
+                cout << "*";
+            }
+
+            cout << endl;
+        }
+
+        // Lower half
+        for(i = n - 1; i >= 1; i--) {
+
+            for(j = 0; j < i; j++) {
+                cout << "*";
+            }
+
+            for(j = 0; j < 2 * (n - i); j++) {
+                cout << " ";
+            }
+
+            for(j = 0; j < i; j++) {
+                cout << "*";
+            }
+
+            cout << endl;
+        }
+    }
+};
+int main() {
+    Solution obj;
+    int n;
+    cin >> n;
+    obj.pattern20(n);
+}

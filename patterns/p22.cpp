91ecb1349e2630e066a14f72dadf96bb0b42b307
Pari <paripurswani912@gmail.com>
2026-08-21 18:02:17 +0530

    pattern 22

---
+++ patterns/p22.cpp
@@ -0,0 +1,36 @@
+//Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+//5 5 5 5 5 5 5 5 5 
+//5 4 4 4 4 4 4 4 5 
+//5 4 3 3 3 3 3 4 5 
+//5 4 3 2 2 2 3 4 5 
+//5 4 3 2 1 2 3 4 5 
+//5 4 3 2 2 2 3 4 5 
+//5 4 3 3 3 3 3 4 5
+//5 4 4 4 4 4 4 4 5 
+//5 5 5 5 5 5 5 5 5
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void pattern22(int n) {
+        int i,j;
+        for(i=0;i<2*n-1;i++){
+            for(j=0;j<2*n-1;j++){
+                int top=i;
+                int left=j;
+                int right=(2*n-2)-j;
+                int bottom=(2*n-2)-i;
+                cout<<n-min(min(top,left),min(right,bottom));
+                cout<<" ";
+            }
+            cout<<endl;
+        }
+
+    }
+};
+int main() {
+    Solution obj;
+    int n;
+    cin >> n;
+    obj.pattern22(n);
+}
